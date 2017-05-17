87b245074f4a95fd3570248d188aab271491c7b6
Sophia Di <backwardsxmarathon@gmail.com>
2017-05-17 16:57:00 -0400

    Adds user login form and user signup page which logs user in upon signing up - uses sessions.

--- app.rb
+++ app.rb
@@ -2,5 +2,53 @@ require 'sinatra'
 require 'sinatra/activerecord'
 
 set :database, 'sqlite3:relationaldb.sqlite3'
+set :sessions, true
 
 require './models'
+
+get '/' do
+    p session
+    if session[:user_id]
+        p session
+        @user = User.find(session[:user_id])
+        @blogs = @user.blogs
+        p @user
+        p @blogs
+    end
+    erb :home
+end
+
+get '/sign_up' do
+    erb :sign_up
+end
+
+post '/create_user' do
+    user = User.create(username: params[:username], password: params[:password])
+    session[:user_id] = user.id
+    redirect '/'
+end
+
+post '/login' do
+    user = User.where(username: params[:username]).first
+    if user.password == params[:password]
+        p 'successful login'
+        session[:user_id] = user.id
+        p session[:user_id]
+        if session[:user_id]
+            p 'this works'
+        end
+    end
+    redirect '/'
+end
+
+get '/read_post' do
+    @post = Blog.find(params[:post_id])
+    erb :read_post
+end
+
+#
+# get '/profile' do
+#     @user = User.find(session[:user_id])
+#     @blogs = @user.blogs
+#     erb :blogs
+# end
